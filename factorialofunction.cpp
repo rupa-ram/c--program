4e79c5b0d42a135b2240eb906cea81a12569cf68
rupa-ram <125654816+rupa-ram@users.noreply.github.com>
2023-02-22 08:36:38 +0530

    Add files via upload

---
+++ factorialofunction.cpp
@@ -0,0 +1,20 @@
+// function of factorial
+#include<stdio.h>
+int main()
+{
+	int n,f=1;
+	int fac(int);
+	{	
+	scanf("%d",&n);
+	f=fac(n);
+	printf("%d",&f);
+}
+   	int fac(int n);
+   	{
+   		int f=1;
+   		while(n>0)
+   		 f=f*1;
+   		 n--;
+	   }
+	   return f;
+   }
